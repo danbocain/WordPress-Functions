cb7bc2fa89737951f2a6095bcbd4fc85e29880e8
Dan Bochichio <thedan1984@gmail.com>
2018-09-27 08:37:33 -0400

    Create custom-login-page.php

---
+++ custom-login-page.php
@@ -0,0 +1,12 @@
+<?php
+
+/* Add to functions and change stylesheet location
+
+function custom_login_css() {
+echo '<link rel="stylesheet" type="text/css" href="'.get_stylesheet_directory_uri().'/login/login-styles.css" />';
+}
+
+add_action('login_head', 'custom_login_css');
+
+
+?>
