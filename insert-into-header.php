c8c1952ed55d31d741535ecd13470bf39fb30b28
Dan Bochichio <thedan1984@gmail.com>
2020-04-15 11:11:22 -0400

    Create insert-into-header.php

---
+++ insert-into-header.php
@@ -0,0 +1,10 @@
+//Add to your child theme's function.php file
+/* Insert tracking codes into site header*/
+add_action('wp_head', 'bocain_tracking_codes');
+function bocain_tracking_codes(){
+?>
+
+//Your Custom Code Here
+
+<?php
+};
