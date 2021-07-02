3fe8536c8c424c74ec5ea4e63e49c49a0a81600b
Dan Bochichio <thedan1984@gmail.com>
2021-07-02 16:30:49 -0400

    Update insert-into-footer.php

--- insert-into-footer.php
+++ insert-into-footer.php
@@ -1,11 +1,16 @@
+<?php
+
+//BEGIN COPY: 
+
 /* Insert tracking codes into site footer*/
 add_action('wp_footer', 'bocain_tracking_codes_footer');
 function bocain_tracking_codes_footer(){
 ?>
 
-<!-- your custom code here -->
 
+<!-- Your Custom Code Here --->
 
-?><?php
-}; //end footer insert function
+<?php
+};
 
+//END COPY
